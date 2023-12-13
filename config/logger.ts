d48a4558678b7648551f65249526fe1a258f8845
technologicaljerry <cserajatverma@gmail.com>
2023-12-13 22:22:23 +0530

    Update logger.ts

--- config/logger.ts
+++ config/logger.ts
@@ -2,7 +2,7 @@ import logger from "pino";
 import dayjs from "dayjs";
 import config from "config";
 
-const level = config.get<string>("logLevel");
+const level = config.get<string>('logLevel');
 
 const log = logger({
   transport: {
