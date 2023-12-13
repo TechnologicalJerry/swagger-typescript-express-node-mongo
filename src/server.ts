7c5b59ba2c23d4b2bcc4b8827948ccab4a5d8ec7
technologicaljerry <cserajatverma@gmail.com>
2023-12-13 22:14:08 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import config from "config";
+import databaseConnection from "../config/databaseConfig";
 
 require("dotenv").config();
 
 const app = express();
 
-const HOST = config.get('host');
+const HOST = config.get("host");
 
-const PORT = config.get('port');
+const PORT = config.get("port");
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING on HOST:${HOST} PORT:${PORT}`);
+  databaseConnection();
 });
