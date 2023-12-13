8eed830753bd382741a9b31339b5f66545abc54b
technologicaljerry <cserajatverma@gmail.com>
2023-12-13 22:27:10 +0530

    Update default.ts

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,7 @@
 export default {
-  host: "localhost",
+  host: process.env.SERVER_HOST_NAME,
   port: 5050,
   dbUrl: "mongodb://localhost:27017/swagger-typescript-express-node-mongo",
   logLevel: 'info'
 };
+ 
